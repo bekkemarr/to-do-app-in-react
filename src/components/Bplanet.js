cf593005e77e50e081e2084345fc708bca1584ea
mrsmarr <bekkemarr@gmail.com>
2022-10-25 20:32:54 -0600

    imgs no good

--- src/components/Bplanet.js
+++ src/components/Bplanet.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import image from '../imgs/IMG_7847.PNG'
+// import image from '../imgs/IMG_7847.PNG'
 
 const Bplanet = () => {
   return (
     <div className='centered'>
-        <div className="palnet">
-            <img src={image} alt="" className='image' />
+        <div className="planet">
+            {/* <img src={image} alt="" className='image' /> */}
         </div>
     </div>
   )
--- src/components/Rplanet.js
+++ src/components/Rplanet.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import image from '../imgs/IMG_7848.PNG'
+// import image from '../imgs/IMG_7848.PNG'
 
 const Rplanet = () => {
   return (
     <div className='centered'>
-        <div className="palnet">
-            <img src={image} alt="" className='image' />
+        <div className="planet">
+            {/* <img src={image} alt="" className='image' /> */}
         </div>
     </div>
   )
